912d3bd0ddfcb3ada563716b93ccf64ea8c702c7
JesusBlan <jezublanco@gmail.com>
2025-02-15 13:17:43 +0100

    added a list of what i need in pseudocode so i dont get lost on the way

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./styles.css";
-import { greeting } from "./home.js";
+import {  } from "./home.js";
 
-console.log(greeting);
+
+// add a class to construc objects with the info i want from the api
+// add the async function to fetch info from the api
+// add a function to construct a new object with the fetched info
+// a function to add the object to the dom and diplay it
+// make the search bar usable
+// add a default locations to display at start
+// ...
+// profit(hopefully...)
+
