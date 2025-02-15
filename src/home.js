0f89edaca32b88f2b38299e54a7f10f5dbfbc70e
JesusBlan <jezublanco@gmail.com>
2025-02-15 13:29:33 +0100

    added a class with all the info to fetch from the api

--- src/home.js
+++ src/home.js
@@ -1,1 +1,16 @@
-export const greeting = "its working";
+
+class location {
+    constructor(place, temp, maxTemp, minTemp, precipitation, precipitationProb, humidity, windSpeed, conditions , description, icon){
+        this.place = place;
+        this.temp = temp;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.precipitation = precipitation;
+        this.precipitationProb = precipitationProb;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.conditions = conditions;
+        this.description = description;
+        this.icon = icon;
+    }
+}
