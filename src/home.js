6981b94b7e5b5414fe2ef990365f3c4cd16c7c25
JesusBlan <jezublanco@gmail.com>
2025-02-16 13:18:16 +0100

    added an asynchronous function to fecth info from the api and create an object using the class location

--- src/home.js
+++ src/home.js
@@ -1,21 +1,42 @@
+storage = [];
 
 class location {
-    constructor(place, temp, maxTemp, minTemp, precipitation, precipitationProb, humidity, windSpeed, conditions , description, icon){
+    constructor(place, temp, feelTemp, precipitation, precipitationProb, humidity, windSpeed, conditions , description){
         this.place = place;
         this.temp = temp;
-        this.maxTemp = maxTemp;
-        this.minTemp = minTemp;
+        this.feelTemp = feelTemp;
         this.precipitation = precipitation;
         this.precipitationProb = precipitationProb;
         this.humidity = humidity;
         this.windSpeed = windSpeed;
         this.conditions = conditions;
         this.description = description;
-        this.icon = icon;
     }
 }
 
-function saveData(place, temp, maxTemp, minTemp, precipitation, precipitationProb, humidity, windSpeed, conditions, description, icon){
-    let data =  new location(place, temp, maxTemp, minTemp, precipitation, precipitationProb, humidity, windSpeed, conditions, description, icon);
-    return data
+function saveData(place, temp, feelTemp, precipitation, precipitationProb, humidity, windSpeed, conditions, description){
+    let data =  new location(place, temp, feelTemp, precipitation, precipitationProb, humidity, windSpeed, conditions, description);
+    storage.push(data);
 }
+
+
+fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cadiz?unitGroup=us&key=PWUP89HBNGTGDR8K4BYC8LYPX&contentType=json', {mode: 'cors'})
+.then(function(response) {
+    return response.json();
+})
+.then(function(response) {
+    let place = response.address;
+    let temp = response.currentConditions.temp;
+    let feelTemp = response.currentConditions.feelslike;
+    let precipitation = response.currentConditions.precip;
+    let precipitationProb = response.currentConditions.precipprob;
+    let humidity = response.currentConditions.humidity;
+    let windSpeed = response.currentConditions.windspeed;
+    let conditions = response.currentConditions.conditions;
+    let description = response.description;
+
+    saveData(place, temp, feelTemp, precipitation, precipitationProb, humidity, windSpeed, conditions, description)
+    console.log(response)
+    console.log(storage)
+});
+
